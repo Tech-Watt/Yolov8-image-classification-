cfe90fbd8e0ad7330c59122c126beed9d84d3c75
Nanor Felix Sam <donoasis88@gmail.com>
2024-01-28 01:24:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-datapath = r'C:\Users\Admin\Desktop\CatDog'
+datapath = r'place path to your root folder'
 model = YOLO('yolov8n-cls.pt')
 result = model.train(data = datapath,epochs = 500,imgsz = 640)
